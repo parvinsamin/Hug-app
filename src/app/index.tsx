e3ab82ff0f28cb60dd596bc856d3376ea250f6de
parvinkh <parkhcom1387@gmail.com>
2026-04-27 08:14:18 +0330

    Start style

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,6 +2,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
 import AppNavigator from './navigation/AppNavigator';
 
+
 export default function App() {
     return (
         <NavigationContainer>
